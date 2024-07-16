5e23b380c3d3e17698bcfe97f349180a4d22f182
zyzz <149920414+rumirinbrah@users.noreply.github.com>
2024-07-16 22:44:43 +0530

    Create Frequent.groovy
    Frequently needed dependencies

---
+++ Frequent.groovy
@@ -0,0 +1,9 @@
+//usually needed
+
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.7")
+    implementation("androidx.compose.ui:ui:1.6.7")
+    implementation("androidx.compose.material:material:1.6.7")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.7")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.1")
+    implementation("io.coil-kt:coil-compose:2.6.0")
