cb6ae2eb8aab33ff749fa9b59105706a09ed9a30
zyzz <149920414+rumirinbrah@users.noreply.github.com>
2024-07-16 22:43:29 +0530

    Create ExoPlayer.groovy
    Exo player

---
+++ ExoPlayer.groovy
@@ -0,0 +1,6 @@
+//EXO PLAYER
+    implementation("androidx.media3:media3-exoplayer:1.3.1")
+    implementation("androidx.media3:media3-exoplayer-dash:1.3.1")
+    implementation("androidx.media3:media3-ui:1.3.1")
+    implementation("androidx.media3:media3-common:1.3.1")
+    implementation("androidx.media3:media3-session:1.3.1")
