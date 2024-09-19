cf7dc1ed89058bf007012a8353cd7c11e493a34c
zyzz <149920414+rumirinbrah@users.noreply.github.com>
2024-09-19 10:17:05 +0530

    ksp

--- room.groovy
+++ room.groovy
@@ -1,4 +1,11 @@
+
+plugins{
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+}
+plugins{
+    id("com.google.devtools.ksp")
+}
 //ROOM
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
